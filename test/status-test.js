2b78a60d2b7f4b614dbc34f7f7bd8b5082009ca7
Dmitry Tarasev <>
2018-10-25 13:19:17 -0400

    Fixed up test to pass, schema was wrong and JSON not being parsed

--- test/status-test.js
+++ test/status-test.js
@@ -3,7 +3,7 @@ chai.use(require('chai-json-schema'));
 
 var assert = chai.assert;
 
-var Status = require('./../src/status')
+var Status = require('./../src/status');
 
 var statusSchema = {
     title: 'Status Schema',
@@ -20,18 +20,19 @@ var statusSchema = {
         },
         facing: {
             type: 'string',
-            minLength: 2,
-            maxLength: 3
+            minLength: 1,
+            maxLength: 1
         },
     }
 };
 
 
-describe('StatusCheck', function(){
-    it('Return the current position (x,y) of the rover and it\'s facing (N,S,E,W)', function(){
+describe('StatusCheck', function () {
+    it('Return the current position (x,y) of the rover and it\'s facing (N,S,E,W)', function () {
         var status = new Status();
-        
 
-        assert.jsonSchema(status.getStatus(), statusSchema);
+        var testData = JSON.parse(status.getStatus());
+
+        assert.jsonSchema(testData, statusSchema);
     })
 });
